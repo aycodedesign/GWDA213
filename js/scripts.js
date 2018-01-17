ae0cbc8c0f69b78594842011367a174ac5141c45
Student <student@2035-01-301-15.local>
2018-01-16 16:07:51 -0800

    Week 1 complete

--- js/scripts.js
+++ js/scripts.js
@@ -25,3 +25,20 @@ $(document).ready(function() {
   });
 
 });
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+	if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+		document.getElementById("scrollUp").style.display = "block";
+	} else {
+		document.getElementById("scrollUp").style.display = "none";
+	}
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+	document.body.scrollTop = 0;
+	document.documentElement.scrollTop = 0;
+}
